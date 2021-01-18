183d55f9779b43efe49ea7d93e3d35a7e9fcbd56
laetho <thorsplass@gmail.com>
2021-01-18 07:39:17 +0100

    Updated spec

--- api/v1/policyengineendpoint_types.go
+++ api/v1/policyengineendpoint_types.go
@@ -23,13 +23,63 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+type PolicyEngineModule string
+
 // PolicyEngineEndpointSpec defines the desired state of PolicyEngineEndpoint
 type PolicyEngineEndpointSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Name    string    `json:"name"`
+	Host    string    `json:"host"`
+	Path    string    `json:"string"`
+	Modules []*Module `json:"modules"`
+}
 
-	// Foo is an example field of PolicyEngineEndpoint. Edit policyengineendpoint_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+type Module struct {
+	Name      PolicyEngineModule      `json:"name"`
+	Order     int                     `json:"order"`
+	Consumers []*PolicyEngineConsumer `json:"consumers,omitempty"`
+	General   *General                `json:"general,omitempty"`
+	Default   *Arguments              `json:"default,omitempty"`
+}
+
+type General struct {
+	Optional bool   `json:"optional,omitempty"`
+	Realm    string `json:"realm,omitempty"`
+	// HMAC
+	Skew string `json:"skew,omitempty"`
+	// oAuth2
+	CallBack string `json:"callback,omitempty"`
+	ClientId string `json:"client_id,omitempty"`
+	// Client secret from provider
+	Secret   string `json:"secret,omitempty"`
+	Provider string `json:"provider,omitempty"`
+	// Custom JWT signing secret
+	JWTSecret string `json:"jwt_secret,omitempty"`
+	TTL       string `json:"ttl,omitempty"`
+	Cookie    string `json:"cookie,omitempty"`
+}
+
+type Arguments struct {
+	// Endpoint backend related
+	Url          string `json:"url,omitempty"`
+	PreserveHost bool   `json:"preserve_host,omitempty"`
+	StripPath    bool   `json:"strip_path,omitempty"`
+	// Authentication related
+	Secret   string `json:"secret,omitempty"`
+	Password string `json:"password,omitempty"`
+	Issuer   string `json:"issuer,omitempty"`
+	// IP or HTTP verb allow deny
+	Order string `json:"order,omitempty"`
+	Allow string `json:"allow,omitempty"`
+	Deny  string `json:"deny,omitempty"`
+	// Rate limit arguments
+	Hour   int `json:"hour,omitempty"`
+	Minute int `json:"minute,omitempty"`
+	Second int `json:"second,omitempty"`
+}
+
+type PolicyEngineConsumer struct {
+	Name  string `json:"name"`
+	EMail string `json:"email,omitempty"`
 }
 
 // PolicyEngineEndpointStatus defines the observed state of PolicyEngineEndpoint
